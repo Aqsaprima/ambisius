3ea7d54567448d866ce7fcfd45e7d79cacec3513
Aqsaprima <aqsaprima11@gmail.com>
2023-10-16 22:07:04 +0700

    progres bikin dashboard

---
+++ src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class=" bg-orange-500 h-20 w-screen flex flex-row place-items-center place-content-start px-12">
+    <div class="">
+      <img src="" alt="">
+      <p>@yourusername</p>
+    </div>
+    <input type="text" name="" id="" placeholder="Cari Pertanyaan" class=" bg-gray-200 placeholder:text-black px-6 rounded-3xl w-2/5 h-10 justify-self-center">
+  </header>
+</template>
---
+++ src/views/dashboard.vue
@@ -0,0 +1,7 @@
+<script setup>
+import atas from '../components/header.vue'
+</script>
+
+<template>
+  <atas></atas>
+</template>
